3509e49e4fbecfec427fec42d8085f2890627024
Wojciech Homa <wojtih@gmail.com>
2017-03-12 16:00:09 +0100

    Added products with faker seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+  Product.create! name: Faker::Commerce.product_name, description: Faker::Commerce.color, price: Faker::Commerce.price
+end
